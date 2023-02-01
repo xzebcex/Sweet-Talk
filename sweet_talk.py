541b967e0cc1eacc10c02e001ac658ddfd9274b9
Aman Pandit <amanpandit3027@gmail.com>
2023-02-01 05:42:52 +0530

    Create sweet_talk.py

---
+++ sweet_talk.py
@@ -0,0 +1,28 @@
+# Sweet Talk.
+# This program generate random pickup lines.
+
+import random
+
+pickup_lines = ['So, aside from taking my breath away, what do you do for a living?',
+                ' I ought to complain to Spotify for you not being named this week\'s hottest single.',
+                'Your eyes are like the ocean; I could swim in them all day.',
+                'When I look in your eyes, I see a very kind soul.',
+                'Do you have a name, or can I just call you \'mine\'?',
+                'You\'re so fine, you made me forget my pickup line.',
+                'Do you have a map? Because I just got lost in your eyes.',
+                'You owe me a drink. Because when I saw you, I dropped mine.',
+                'They say nothing lasts forever, so will you be my nothing?',
+                'If you were a song, you\'d be the best single on the album.',
+                'Damm girl Are you a washing machine? Cuz you talk a lot of shit for a washing machine.',
+                'My love for you is like diarrhea. I just can\'t hold it in.',
+                'Your eyes are as blue as  water.',
+                'Are you ice cream? Because your face looks like rocky road.',
+                'Hey, how much?',
+                'Are you the future? Because you\'re looking hopeless and bleak.',
+                'Are you a motorcycle? Because I\'d like to ride you all day, and then sell you for a newer model.',
+                'Are you a mosquito? Because you\'re so annoying!',
+                'You are so beautiful,You remind me of my brother.',
+                'Are you pi? Because you\'re being irrational and this conversation is going in circles.']
+
+
+print(random.choice(pickup_lines))
